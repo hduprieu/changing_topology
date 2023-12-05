21c915e2e5f018abc7e70b8df3c6be3c4a97a9b9
hduprieu <hduprieu@gmail.com>
2023-12-05 02:23:40 +0100

    sdfg

--- Cifar_experiment/cifar_TVE_train_cuda.py
+++ Cifar_experiment/cifar_TVE_train_cuda.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 import torch.optim as optim
 
-start_lr = 1
+start_lr = 1.01
 effective_batch_size = 16
 num_workers = 8
 batch_size = num_workers * effective_batch_size
